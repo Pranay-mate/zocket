2e0be9839ec7afe129788bcce50dbf3cee03bafb
LAPTOP-747E5JBF\matep <matepranay11@gmail.com>
2022-08-13 17:58:47 +0530

    icons

--- zocket_client/src/component/progressBar.js
+++ zocket_client/src/component/progressBar.js
@@ -15,9 +15,11 @@ export default function ProgressBar(props) {
     <div className="">
         <ul id="progressbar" className="text" style={{"textAlign":"center"}}>
             {total.map((step,id)=>(
+              <>
               <li id={"step"+(id+1)} className={id+1<=count?'active':null}>{id===0?"What you want to do?": id===1? "Choose the Product" :
               id===2? "Campaign Settings" :id===3? "Ready to Go" :null
               }</li>
+              </>
             ))}
         </ul>
     </div>
